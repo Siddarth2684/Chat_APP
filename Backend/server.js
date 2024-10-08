7ca4a449d119211781292429d44925d160bad567
Siddarth2684 <siddarthilk@gmail.com>
2024-10-08 11:15:51 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,7 @@ import { app, server } from "./socket/socket.js";
 //instead of creating a hardcoded value for port 5000 we create a varible and either we use 5000 or we get it from env file 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -28,6 +30,14 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+//this helps the express to serve static files such as html,notiication etc
+app.use(express.static(path.join(__dirname,"/Frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"Frontend", "dist", "index.html"));
+});
 
 // //Test route
 // app.get("/", (req,res) => {
